43c09700e23f5571abd0ddde4d87c7db9078a834
BTomiii <beki.tamas@proton.me>
2025-05-13 20:59:43 +0200

    e

--- install.sh
+++ install.sh
@@ -263,7 +263,7 @@ backup_config
 install_config
 restart_klipper
 
-wget -O - https://raw.githubusercontent.com/BTomiii/klippain-shaketune/BTT_Eddy/install.sh | bash
+#wget -O - https://raw.githubusercontent.com/BTomiii/klippain-shaketune/BTT_Eddy/install.sh | bash
 
 echo "[POST-INSTALL] Everything is ok, Klippain installed and up to date!"
 echo "[POST-INSTALL] Be sure to check the breaking changes on the release page: https://github.com/Frix-x/klippain/releases"
